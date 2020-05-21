ffa706bca359d1adff44e052223e2d7d36b173b6
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2020-05-21 05:57:44 +0200

    Update micrometer library version: 1.5.1

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies
   [[org.clojure/clojure "1.10.1"]
-   [io.micrometer/micrometer-core "1.4.1"]
-   [io.micrometer/micrometer-registry-prometheus "1.4.1" :scope "provided"]
-   [io.micrometer/micrometer-registry-elastic "1.4.1" :scope "provided"]
-   [io.micrometer/micrometer-registry-opentsdb "1.4.1" :scope "provided"]
-   [io.micrometer/micrometer-registry-graphite "1.4.1" :scope "provided"]
-   [io.micrometer/micrometer-registry-influx "1.4.1" :scope "provided"]]
+   [io.micrometer/micrometer-core "1.5.1"]
+   [io.micrometer/micrometer-registry-prometheus "1.5.1" :scope "provided"]
+   [io.micrometer/micrometer-registry-elastic "1.5.1" :scope "provided"]
+   [io.micrometer/micrometer-registry-opentsdb "1.5.1" :scope "provided"]
+   [io.micrometer/micrometer-registry-graphite "1.5.1" :scope "provided"]
+   [io.micrometer/micrometer-registry-influx "1.5.1" :scope "provided"]]
   :repl-options {:init-ns io.resonant.micrometer})
