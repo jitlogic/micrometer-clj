d27b612078463b4d52493bbff08d311e19616f1a
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2020-05-21 19:46:20 +0200

    Stackdriver meter registry

---
+++ src/io/resonant/micrometer/stackdriver.clj
@@ -0,0 +1,37 @@
+(ns io.resonant.micrometer.stackdriver
+  (:require
+    [io.resonant.micrometer :refer [create-registry]]
+    [clojure.java.io :as io])
+  (:import
+    (io.micrometer.stackdriver StackdriverMeterRegistry StackdriverConfig)
+    (com.google.auth.oauth2 GoogleCredentials)
+    (com.google.api.gax.core FixedCredentialsProvider)
+    (com.google.cloud.monitoring.v3 MetricServiceSettings)
+    (io.micrometer.core.instrument.step StepRegistryConfig)
+    (io.micrometer.core.instrument Clock)
+    (java.time Duration)))
+
+(defn- read-credentials [path]
+  (when (nil? path)
+    (throw (ex-info "missing :credentials argument" {})))
+  (FixedCredentialsProvider/create
+    (.createScoped (GoogleCredentials/fromStream (io/input-stream path))
+      (MetricServiceSettings/getDefaultServiceScopes))))
+
+(defmethod create-registry :stackdriver [cfg]
+  (let [creds (read-credentials (:credentials cfg))]
+    (StackdriverMeterRegistry.
+      (reify
+        StackdriverConfig
+        (get [_ _] nil)
+        (projectId [_] (:project-id cfg))
+        (resourceLabels [_] (:resource-labels cfg {}))
+        (resourceType [_] (:resource-type cfg "global"))
+        (credentials [_] creds)
+        StepRegistryConfig
+        (step [_] (Duration/ofMillis (:step cfg 60000)))
+        (enabled [_] (:enabled? cfg true))
+        (numThreads [_] (:num-threads cfg 2))
+        (connectTimeout [_] (Duration/ofMillis (:connect-timeout cfg 1000)))
+        (readTimeout [_] (Duration/ofMillis (:read-timeout cfg 10000))))
+      (Clock/SYSTEM))))
