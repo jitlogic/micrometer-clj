3a444ba726a666d7b013a6bfaec2eda27a6529fd
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2020-05-25 18:36:52 +0200

    Change license to Apache License 2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject io.resonant/micrometer-clj "0.0.3-SNAPSHOT"
   :description "Clojure wrappers for Micrometer library"
   :url "http://github.com/jitlogic/micrometer-clj"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies
   [[org.clojure/clojure "1.10.1"]
